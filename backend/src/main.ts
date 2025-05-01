7868cb14a7657f6cd1a6384a73cb6b9a6e46bd5c
Kyle Aquino <kpaquino2@up.edu.ph>
2025-05-01 11:16:56 +0800

    chore: Setup swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // create new swagger document configuration
+  const config = new DocumentBuilder()
+    .setTitle('Inventory API')
+    .setDescription('Inventory management system')
+    .setVersion('0.1')
+    .build();
+
+  // create swagger document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // setup swagger module
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
