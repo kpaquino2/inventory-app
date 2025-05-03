c2d1e5e9b07dd98cd990ee6b49975ccffc8b9839
Kyle Aquino <kpaquino2@up.edu.ph>
2025-05-03 23:04:14 +0800

    chore: Add hardcoded users to db seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,9 +1,32 @@
 import { faker } from '@faker-js/faker';
-import { PrismaClient } from '@prisma/client';
+import { Position, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const main = async () => {
+  const users = [
+    {
+      name: 'User User',
+      position: Position.USER,
+      username: 'useruser',
+      password: '$2b$10$Q7LC7TC6PA7P6a94OiO7Hu.HZC5GFy4xW5McRo/qiZf8CzCmjfX9q',
+    },
+    {
+      name: 'Auditor User',
+      position: Position.AUDITOR,
+      username: 'auditoruser',
+      password: '$2b$10$azojxgOcj1eBr8u8/Xe7beifI/QwGA1691VBzsgnHhd8e3uh6dkNu',
+    },
+    {
+      name: 'Admin User',
+      position: Position.ADMIN,
+      username: 'adminuser',
+      password: '$2b$10$i7kTa455.N5aU9PYM0yB0eXkpCPtLajuywmxzW3.BLRteeIM390pO',
+    },
+  ];
+
+  await prisma.user.createMany({ data: users });
+
   const categories = 15;
   const productsPerCategories = 20;
   for (let i = 0; i < categories; i++) {
