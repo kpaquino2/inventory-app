1fc1b206e3d922a95e75d84b7c2fb87752d66fa7
Kyle Aquino <kpaquino2@up.edu.ph>
2025-05-05 09:02:35 +0800

    db: Add product indexes

---
+++ backend/prisma/migrations/20250505010202_add_product_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "Product_categoryId_idx" ON "Product"("categoryId");
+
+-- CreateIndex
+CREATE INDEX "Product_desc_idx" ON "Product"("desc");
+
+-- CreateIndex
+CREATE INDEX "Product_name_idx" ON "Product"("name");
